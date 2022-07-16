81e556bd37092cfaa7d3a50167ae89ca71c6ec7a
Hamza Hassan <hamzahassan@clustox.com>
2022-07-16 15:04:50 +0500

    first push

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
-
+// Testing first push to codemagic
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
